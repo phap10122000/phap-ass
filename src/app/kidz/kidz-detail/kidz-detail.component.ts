601dbae1dd2a527879548fbc6644b687ac6e9e16
phap10122000 <trinhhaphap@gmail.com>
2020-03-27 21:46:11 +0700

    2 files modified, 7 added

---
+++ src/app/kidz/kidz-detail/kidz-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kidz-detail',
+  templateUrl: './kidz-detail.component.html',
+  styleUrls: ['./kidz-detail.component.css']
+})
+export class KidzDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/kidz/kidz-list/kidz-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kidz-list',
+  templateUrl: './kidz-list.component.html',
+  styleUrls: ['./kidz-list.component.css']
+})
+export class KidzListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
