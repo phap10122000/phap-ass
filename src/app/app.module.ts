470fa2e2bcfe5639e64f1cee7f8bbb1fc8df17ca
phap10122000 <trinhhaphap@gmail.com>
2020-03-27 21:38:48 +0700

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AdminClothesComponent } from './admin/admin-clothes/admin-clothes.component';
 import { AdminShoesComponent } from './admin/admin-shoes/admin-shoes.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AdminClothesComponent, AdminShoesComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminClothesComponent, AdminShoesComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
